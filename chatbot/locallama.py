a890df43f78c1b70f532c00b79c768ffd8d21a24
Vaibhavsg17 <vaibhav.s221064102@vcet.edu.in>
2024-12-26 01:53:44 +0530

    added OpenAI & Ollama api files

--- chatbot/locallama.py
+++ chatbot/locallama.py
@@ -1,10 +1,12 @@
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-
-import streamlit as st 
-import os 
+from langchain_community.llms import Ollama
+import os
+import streamlit as st
 from dotenv import load_dotenv
+
+load_dotenv()
 
 os.environ['OPENAI_API_KEY']=os.getenv("OPENAI_API_KEY")
 # Langmith tacking
@@ -12,7 +14,6 @@ os.environ['OPENAI_API_KEY']="true"
 os.environ['LANGCHAIN_API_KEY']=os.getenv("LANGCHAIN_API_KEY")
 
 # Promt Template
-
 prompt=ChatPromptTemplate.from_messages(
     [
         ("system","You are a helpful AI assistant, please response to the user queries"),
@@ -21,11 +22,14 @@ prompt=ChatPromptTemplate.from_messages(
 )
 
 # streamlit framework 
-st.title("Langchain Demo With OPENAI API")
+st.title("Langchain Demo With Ollama")
 input_text=st.text_input("Search the topic u want")
 
-# openAI LLm
+# ollama llama2 LLm
 
-llm = ChatOpenAI(model="gpt-3.5-turbo")
+llm = Ollama(model="llama2")
 output_parser=StrOutputParser()
 chain=prompt|llm|output_parser
+
+if input_text:
+    st.write(chain.invoke({"question":input_text}))
